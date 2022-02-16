be3bd870ea7e570c748919daa645611b52004c6a
Várszegi Ádám <varszegi.adam75@gmail.com>
2022-02-16 13:18:28 +0100

    4.feladat kész

--- SorozatKiir/Program.cs
+++ SorozatKiir/Program.cs
@@ -11,7 +11,43 @@ namespace SorozatKiir
         static void Main(string[] args)
         {
             //1.feladat
-
+            //2.feladat
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            //3.feladat
+            Console.WriteLine("Adjon meg egy egynél nagyobb számot");
+            int szam = int.Parse(Console.ReadLine());
+            if (szam < 1)
+            {
+                Console.WriteLine("Hiba");
+            }
+            else
+            {
+                for (int i = 1; i <= szam; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+            //4. feladat
+            Console.WriteLine("Adjon meg egy kezdő értéket");
+            int kezdo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adjon meg egy maximum értéket");
+            int maximum = int.Parse(Console.ReadLine());
+            if (maximum > kezdo)
+            {
+                for (int i = kezdo; i <= maximum; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hiba");
+            }
         }
     }
 }
